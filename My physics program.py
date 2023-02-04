7b70576515fcfcbc5fc972390eb3b4471c36ecb1
CoderKoen <120145485+CoderKoen@users.noreply.github.com>
2023-02-04 13:04:24 -0500

    Update to My code

--- My physics program.py
+++ My physics program.py
@@ -3,7 +3,7 @@
 print("Welcome!")
 
 #User input
-physics_program = input("Choose a physics program (Newton's 2nd Law, Velocity):")
+physics_program = input("Choose a physics program (Newton's 2nd Law, Velocity,Acceleration):")
 
 #Newton's 2nd Law
 if physics_program == "Newtons 2nd Law" or physics_program == "a=F/m":
@@ -17,7 +17,7 @@ if physics_program == "Newtons 2nd Law" or physics_program == "a=F/m":
         print(acc)
         print("m/s^2")
 #Velocity
-elif physics_program == "Velocity" or physics_program == "v=delta displacement/delta time" or physics_program == "v" :
+elif physics_program == "Velocity" or physics_program == "v" :
     solve = input("What would you like to solve (velocity):")
     if solve == "velocity" or solve == "v":
         displacement = input("displacement (m):")
@@ -27,5 +27,14 @@ elif physics_program == "Velocity" or physics_program == "v=delta displacement/d
         vel = num_displacement / num_time
         print(vel)
         print("m/s")
+#Acceleration
+elif physics_program == "Acceleration" or physics_program == "a"
+    Change_v_acc = input("Change in velocty (m/s):")
+    num_Change_v_acc = int(Change_v_acc)
+    Change_t_acc = input("Change in time (t):")
+    num_Change_t_acc = int(Change_t_acc)
+    a = Change_v_acc/Change_t_acc
+    print(a)
+    print(m/s^2)
 
        
