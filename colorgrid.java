98cff566a4cbd523f9559417c86946244e733b73
CoderKoen <120145485+CoderKoen@users.noreply.github.com>
2023-07-28 10:25:51 -0400

    Add files via upload
    You need processing and to get it running, copy the java code into your sketch and run!

---
+++ colorgrid.java
@@ -0,0 +1,22 @@
+//this is Java NOT JavaScript
+//using Processing to make this :)
+void setup(){
+  size(400,400);
+}
+float x = 0;
+float y = 0;
+float squareSize = 20;
+void draw(){
+ //background(0); 
+ fill(x,y,0);
+ noStroke();
+ square(x,y,squareSize);
+ x += squareSize;
+ if (x > 400){
+   x = 0;
+   y += squareSize;
+ }
+ if (y > 400) {
+  noLoop();
+ }
+}
