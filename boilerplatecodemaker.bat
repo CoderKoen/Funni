221537fa8a5be927aa71b5ce151d379ec3455b05
Coderofthings <120145485+CoderKoen@users.noreply.github.com>
2023-08-03 14:59:02 -0400

    Update boilerplatecodemaker.bat

--- boilerplatecodemaker.bat
+++ boilerplatecodemaker.bat
@@ -2,61 +2,63 @@
 echo Making your file...
 set /p Input=What do you want your file to be named: 
 set /p Dir=In which directory do you want to save it: 
-set /p Code=What programing languge (C,C#,HTML):
+set /p Code=What programming language (C, C#, HTML):
 
 
-if "%Code%"=="C"||"%Code%"=="c" (
-(
-    echo #include ^<stdio.h^>
-    echo.
-    echo int main^(^) {
-    echo    printf("Hello World")
-    echo    return 0;
-    echo }
+if /i "%Code%"=="C" (
+    (
+        echo #include ^<stdio.h^>
+        echo.
+        echo int main^(^) {
+        echo    printf("Hello World")
+        echo    return 0;
+        echo }
 
-) > "%Dir%\%Input%.c"
-echo Done!
+    ) > "%Dir%\%Input%.c"
+    echo Done!
 )
 
 
-if "%Code%"=="C#" || "%Code%"=="c#" (
-(
-    echo using System;
-    echo.
-    echo namespace Stuff
-    echo ^{
-    echo    class Program
-    echo   ^{
-    echo        static void Main(string args[])
-    echo       ^{
-    echo            Console.WriteLine("Hello World");
-    echo       ^}
-    echo   ^}
-    echo ^}
-
-) > "%Dir%\%Input%.cs"
-echo Done!
+if /i "%Code%"=="C#" (
+    cd %Dir%
+    dotnet new console -n %Input%
+    echo Done!
 ) 
 
 
-if "%Code%"=="HTML"||"%Code%"=="html" (
-(
-    echo <!DOCTYPE html>
-    echo.
-    echo <html>
-    echo    <head>
-    echo        <title></title>
-    echo        <style>
-    echo        </style>
-    echo    </head>
-    echo    <body>
-    echo        <h1>Hello World!</h1>
-    echo    </body>
-    echo </html>
-    
-)> "%Dir%\%Input%.html"
-echo Done!
+if /i "%Code%"=="HTML" (
+    (
+        echo ^<!DOCTYPE html^>
+        echo.
+        echo ^<html^>
+        echo    ^<head^>
+        echo        ^<title^>^</title^>
+        echo        ^<style^>
+        echo        ^</style^>
+        echo    ^</head^>
+        echo    ^<body^>
+        echo        ^<h1^>Hello World!^</h1^>
+        echo    ^</body^>
+        echo ^</html^>
+        
+    ) > "%Dir%\%Input%.html"
+    echo Done!
 )
+
+if /i "%Code%"=="Java" (
+    mkdir %Input%
+    cd %Input%
+    mkdir src
+
+    (
+        echo public class %Input% {
+        echo.
+        echo    public static void main^(String^[^] args^) {
+        echo        System.out.println("Hello, World!");
+        echo    }
+        echo }
+    ) > "%Dir%\%Input%\src\%Input%.java"
+) 
 
 
 
